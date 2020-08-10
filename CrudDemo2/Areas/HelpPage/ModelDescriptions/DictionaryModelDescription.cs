155381bff04c85ff8240397e4d443f5d874e10dc
pcuser <aiksiforsoft@gmail.com>
2020-08-10 08:06:02 +0200

    Add project files.

---
+++ CrudDemo2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CrudDemo2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CrudDemo2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrudDemo2.WebApiApplication" Language="C#" %>
